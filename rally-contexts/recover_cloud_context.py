bda96124dd9ca53d77ed1370cd4a6fcf883c3667
Kyrylo Romanenko <kromanenko@mirantis.com>
2015-03-18 13:37:26 +0200

    Small typo fix: shaker instead of shaiker

--- rally-contexts/recover_cloud_context.py
+++ rally-contexts/recover_cloud_context.py
@@ -32,7 +32,7 @@ class CloudNodesContext(base.Context):
         """This method is called before the task start"""
         self.context["actions"] = self.ACTIONS
 
-        # done_actions contains information about name of shaiker_id
+        # done_actions contains information about name of shaker_id
         # and action name which were executed, example:
         # self.context["done_actions"] = [{"name": "node-1", "command": "ls"}]
         self.context["done_actions"] = []
@@ -41,5 +41,5 @@ class CloudNodesContext(base.Context):
         """This method is called after the task finish"""
         for action in self.context["done_actions"]:
             ## we need to import shaker somehow :)
-            shaiker.run_command_on_node(action["node"],
+            shaker.run_command_on_node(action["node"],
                                         ACTIONS[action["command"]]["undo"])
