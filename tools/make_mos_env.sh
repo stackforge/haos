acd72879f79441b421d20a46b98b512035c5f79d
Monty Taylor <mordred@inaugust.com>
2015-10-17 16:03:09 -0400

    Retire stackforge/haos

--- tools/make_mos_env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -xe
-
-tools/verify.sh || exit 1
-tools/install_rally.sh || exit 1
-tools/install_haos_agents.sh || exit 1
-tools/build_image.sh || exit 1
--- tools/run_rally.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TOP_DIR=$(cd $(dirname "$0") && pwd)
-
-SCENARIO=$1
-
-if [ ! -z ${SCENARIO} ]; then
-    rally --debug --plugin-path ${TOP_DIR}/../haos/rally/context,${TOP_DIR}/../haos/rally/plugin task start ${SCENARIO}
-fi
